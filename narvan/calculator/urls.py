e8591d630cb9376c64ce90807738e5b79b3967ad
el-rizado <amirhosseinmohseni1996@yahoo.com>
2021-01-21 18:28:25 +0330

    cerate url and view for fibonacci function

--- narvan/calculator/urls.py
+++ narvan/calculator/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('fibonacci/<int:number>', views.fibonacci, name='fibonacci'),
+    # path('factorial/<int:number>', views.factorial, name='factorial'),
 
 ]
--- narvan/calculator/views.py
+++ narvan/calculator/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 
-def index(request):
-    return HttpResponse("yes")
+def fib(number):
+    if number < 2:
+        return number
+    return fib(number - 2) + fib(number - 1)
+
+
+def fibonacci(request, number):
+    # return render(request, 'base.html')
+    if request.method == 'GET':
+        if number >= 0:
+            response = {f'fibonacci( {number} )': fib(number-1)}
+        else:
+            response = {'error': 'number must be positive'}
+    return JsonResponse(response)
+
+#
+# def fac(number):
+#     fac_sum = 1
+#     for i in range(1, number+1):
+#         fac_sum *= i
+#     return fac_sum
+#
+#
+# def factorial(request, number):
+#     if request.method == 'GET':
+#         if number >= 0:
+#             response = {f'factorial( {number} )': fac(number)}
+#         else:
+#             response = {'error': 'number must be positive'}
+#     return JsonResponse(response)
